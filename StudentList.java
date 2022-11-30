727e625aa62b38d4e57c1ea6d4fca2f2c430a9bf
Md. Abu Tofail Ahamed <abutofailahamedcse1@gmail.com>
2022-11-30 20:02:47 +0600

    Update StudentList.java

--- StudentList.java
+++ StudentList.java
@@ -1,95 +1,137 @@
 import java.io.*;
 import java.text.*;
 import java.util.*;
-public class StudentList {
-	public static void main(String[] args) {
 
-//		Check arguments
-		if(args[0].equals("a")) {
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			String i[] = r.split(",");			
-			for(String j : i) { System.out.println(j); }
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");
-		}
-		else if(args[0].equals("r")) 
+public class StudentList 
+{
+	public static void main(String[] args) 
+
+	{
+		if (args.length != 1)   // handle invalid number of arguments.
+        {                 
+            System.err.println("invalid"); 
+        }
+		
+		// section: print all
+		else if(args[0].equals(Constant.printAll))  
 		{
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			System.out.println(r);
-			String i[] = r.split(",");	
-			Random x = new Random();
-				int y = x.nextInt();
-					System.out.println(i[y]);
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");			
+			System.out.println(Constant.load);		
+			
+			try 
+			{
+				for(String j : readFile().readLine().split(Constant.comma)) 
+				{ 
+					System.out.println(j); 
+				}
+			} 
+			catch (Exception e)
+			{
+			} 
+			
+			System.out.println(Constant.loaded);
 		}
-		else if(args[0].contains("+")){
-			System.out.println("Loading data ...");			
-			try {
-			BufferedWriter s = new BufferedWriter(
-					new FileWriter("students.txt", true));
-			String t = args[0].substring(1);
-	        Date d = new Date();
-	        String df = "dd/mm/yyyy-hh:mm:ss a";
-	        DateFormat dateFormat = new SimpleDateFormat(df);
-	        String fd= dateFormat.format(d);
-			s.write(", "+t+"\nList last updated on "+fd);
-			s.close();
-			} catch (Exception e){}
+		
+		// section: Print random data
+		else if(args[0].equals(Constant.random)) 
+		{
+			System.out.println(Constant.load);			
+			try 
+			{				
+				String nameOfStudents[] = readFile().readLine().split(",");
+				
+				System.out.println(nameOfStudents[new Random().nextInt(nameOfStudents.length)]);
+			} 
+			catch (Exception e)
+			{
+			} 
+			System.out.println(Constant.loaded);			
+		}
+		
+		// section: add new data  
+		else if(args[0].contains(Constant.add))
+		{
+			System.out.println(Constant.load);			
+			try 
+			{
+				BufferedWriter bufferedWriter = writeFile();
+				
+				String inputData = args[0].substring(1);
+		        
+		        String dateFormat = new SimpleDateFormat(Constant.df).format(new Date());
+		        bufferedWriter.write(Constant.comma + inputData + Constant.lastUpdate + dateFormat);
+		        bufferedWriter.close();
+			} 
+			catch (Exception e)
+			{
+			}
 							
-			System.out.println("Data Loaded.");	
+			System.out.println(Constant.loaded);	
 		}
-		else if(args[0].contains("?")) 
+		
+		// case: search data
+		else if(args[0].contains(Constant.search)) 
 		{
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			String i[] = r.split(",");	
-			boolean done = false;
-			String t = args[0].substring(1);
-			for(int idx = 0; idx<i.length && !done; idx++) {
-				if(i[idx].equals(t)) {
-					System.out.println("We found it!");
-						done=true;
-				}
-			}
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");				
-		}
-		else if(args[0].contains("c")) 
-		{
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String D = s.readLine();
-			char a[] = D.toCharArray();			
-			boolean in_word = false;
-			int count=0;
-			for(char c:a) {
-				if(c ==' ') 
+			System.out.println(Constant.load);			
+			try 
+			{
+				String nameOfStudents[] = readFile().readLine().split(Constant.comma);	
+				
+				boolean found = false;
+				
+				for(String name : nameOfStudents)
 				{
-					if (!in_word) {	count++; in_word =true;	}
-					else { in_word=false;}			
+					if(name.equals(args[0].substring(1))) 
+					{
+						System.out.println(Constant.found);
+						found = true;
+					}
 				}
-			}
-			System.out.println(count +" word(s) found " + a.length);
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");				
+			} catch (Exception e)
+			{
+			} 
+			System.out.println(Constant.loaded);				
+		}
+		
+		// case: count data
+		else if(args[0].contains(Constant.count)) 
+		{
+			System.out.println(Constant.load);			
+			try 
+			{					
+				System.out.println(readFile().readLine().split(Constant.comma).length + Constant.wordFound);
+			} 
+			catch (Exception e)
+			{
+			} 
+			System.out.println(Constant.loaded);				
+		}
+		
+		// handle invalid arguments.
+		else
+		{
+			System.err.println(Constant.invalid);
+            System.err.println(Constant.exit);
+            System.exit(2);
 		}
 	}
+	
+    // Static method called: readFile() --> File Read Object
+    private static BufferedReader readFile() throws FileNotFoundException 
+    {
+        BufferedReader bufferedReader = new BufferedReader(
+        		new InputStreamReader
+        				(new FileInputStream(Constant.studentsList)));
+        
+        return bufferedReader;
+    }
+
+    // Static method called: writeFile() --> File Write Object
+    private static BufferedWriter writeFile() throws IOException 
+    {
+        BufferedWriter bufferedWriter = new BufferedWriter(
+        		new FileWriter(
+        				Constant.studentsList, true));
+        
+        return bufferedWriter;
+    }
 }
